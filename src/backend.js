a4eb7bae0539864b70d2c7343d7b6aa486a2c8c7
Lemdorot <lemdorot@gmail.com>
2022-02-08 19:25:26 +0300

    Добавил обработку ошибок в функции отправки и получения данных с сервера

--- src/backend.js
+++ src/backend.js
@@ -1,27 +1,55 @@
 var URLPost = 'https://24.javascript.pages.academy/kekstagram';
 var URLGet = 'https://24.javascript.pages.academy/kekstagram/data';
+var SERVER_ANSWER_OK = 200;
 
-export var upload = function (data, onSuccess) {
+export var upload = function (data, onLoad, onError) {
   var xhr = new XMLHttpRequest();
   xhr.responseType = 'json';
 
   xhr.addEventListener('load', function () {
-    onSuccess(xhr.response);
+    if (xhr.status === SERVER_ANSWER_OK) {
+      onLoad(xhr.response);
+    } else {
+      onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+    }
   });
+
+  xhr.addEventListener('error', function () {
+    onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+  });
+
+  xhr.addEventListener('timeout', function () {
+    onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+  });
+
+  xhr.timeout = 5000;
 
   xhr.open('POST', URLPost);
   xhr.send(data);
 }
 
-export var load = function (onSuccess, onError) {
+export var load = function (onLoad, onError) {
   var xhr = new XMLHttpRequest();
   xhr.responseType = 'json';
 
-  xhr.open('GET', URLGet);
-
   xhr.addEventListener('load', function () {
-    onSuccess(xhr.response);
+    if (xhr.status === SERVER_ANSWER_OK) {
+      onLoad(xhr.response);
+    } else {
+      onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+    }
   });
 
+  xhr.addEventListener('error', function () {
+    onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+  });
+
+  xhr.addEventListener('timeout', function () {
+    onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+  });
+
+  xhr.timeout = 5000;
+
+  xhr.open('GET', URLGet);
   xhr.send();
 };
